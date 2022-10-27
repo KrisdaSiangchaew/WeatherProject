36ca71ce89636f811134e21a1b40bba470ad2bed
KrisdaSiangchaew <67571681+KrisdaSiangchaew@users.noreply.github.com>
2022-10-27 22:10:50 +0700

    Handling POST request

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get("/", function(req, res) {   
-    const url = "https://api.openweathermap.org/data/2.5/weather?id=7759923&units=metric&appid=ba3e2e5807fe83d153bbe802fede1753";
-    
-    https.get(url, (response) => {
-        console.log(response)
-    });
+app.use(bodyParser.urlencoded({extended: true}));
 
-    res.send("Hello from WeatherProject")
+app.get("/", (req, res) => {   
+    res.sendFile(__dirname + "/index.html")
 });
 
-app.listen(3000, function() {
+app.post("/", (req, res) => {
+    const endpoint = "api.openweathermap.org/data/2.5/weather";
+    const query = req.body.cityName;
+    const unit = "metric";
+    const auth = "ba3e2e5807fe83d153bbe802fede1753";
+    const url = `https://${endpoint}?q=${query}&units=${unit}&appid=${auth}`;
+
+    https.get(url, (response) => {
+        // console.log(response.statusCode);
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data)
+
+            const temp = weatherData.main.temp
+            const description = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+            res.write(`<h1>The temperature is ${temp} degrees Celsius.</h1>`)
+            res.write(`<p>The weather in ${query.toUpperCase()} is ${description}.</p>`)
+            res.write(`<img src = ${imageURL}>`)
+            res.send()
+
+            // res.write enable writing multiple lines.
+            // You can send only 1 res.send()
+            // Safari issue: Cannot do inline string interpoloation plus html symbol with res.write()
+        });
+    });
+});
+
+app.listen(3000, () => {
     console.log("Server is running on port 3000.")
 });
